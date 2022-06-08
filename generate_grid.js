b6fdb5ba57479e9d4a0d4596eea13da49e873ffe
rickym-h <richardhull11@gmail.com>
2022-06-08 23:56:11 +0100

    added functionality to update nodes when guessed words

--- generate_grid.js
+++ generate_grid.js
@@ -2,6 +2,40 @@ let NUM_OF_GUESSES = 6;
 let LENGTH_OF_WORD = 5;
 let word = "UNDEFINED";
 
+function updateGuesses(array, word) {
+    let rowToEdit = document.getElementsByClassName("un-guessedRow");
+    if (rowToEdit.length === 0) {
+        console.log("No more rows to update. returning...");
+        return;
+    }
+    rowToEdit = rowToEdit[0];
+
+    console.log(rowToEdit);
+    rowToEdit.classList.toggle("un-guessedRow");
+    rowToEdit.classList.toggle("guessedRow");
+
+    const children = rowToEdit.childNodes;
+    console.assert(children.length === array.length);
+    for (let i = 0; i < word.length; i++) {
+        console.log(children[i]);
+        let node = children[i];
+        node.textContent = word.charAt(i);
+
+        if (array[i] === 0) {
+            continue;
+        }
+        if (array[i] === 1) {
+            node.classList.toggle("node1");
+            node.classList.toggle("node0");
+        }
+        if (array[i] === 2) {
+            node.classList.toggle("node2");
+            node.classList.toggle("node0");
+        }
+    }
+    console.log(array);
+    console.log(word);
+}
 // Takes a user input and a given word, and outputs the comparison array.
 function getOutputArray(input, word) {
     // 0 == not in
@@ -43,11 +77,13 @@ function guessWord(event) {
     let guessWord = textInput.value;
     console.log("guess word = ", guessWord, " real word = ", word);
     if (guessWord.length !== word.length) {
-        // todo show length error
-        return;
+        // todo show length error then return
     }
 
     // todo guess word and update dom elements.
+    let comparisonResult = getOutputArray(guessWord, word);
+    updateGuesses(comparisonResult[0], comparisonResult[1]);
+
 
 }
 const textInput = document.querySelector("#textInput");
@@ -63,10 +99,11 @@ for (let row = 0; row < NUM_OF_GUESSES; row++) {
     // Create a row of nodes and add it to the
     let thisRow = document.createElement("div");
     thisRow.classList.toggle("row");
+    thisRow.classList.toggle("un-guessedRow");
 
     for (let node = 0; node < LENGTH_OF_WORD; node++) {
         let thisNode = document.createElement("div");
-        thisNode.classList.toggle("node2");
+        thisNode.classList.toggle("node0");
         thisRow.appendChild(thisNode);
     }
     rowsOfNodes.appendChild(thisRow);
